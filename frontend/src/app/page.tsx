3880b8331540618ee1c5d93c6ab0487529c08dfe
Daegi Yeo <daegi0923@gmail.com>
2025-08-15 22:15:34 +0900

    chore : tailwind 의존성 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,24 @@
-"use client";
-import styles from "./page.module.css";
-import {WalletButton} from "@/components/Wallet/Button/WalletButton";
-import {useState} from "react";
-import {useWalletAccountStore} from "@/components/Wallet/Account/auth.hooks";
+'use client';
+import { WalletButton } from '@/components/Wallet/Button/WalletButton';
+import { useState } from 'react';
+import { useWalletAccountStore } from '@/components/Wallet/Account/auth.hooks';
 
 export default function Home() {
-    const { account } = useWalletAccountStore();
-    const [count, setCount] = useState(0);
+  const { account } = useWalletAccountStore();
+  const [count, setCount] = useState(0);
 
-    return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-          {account?<button className={styles.button} onClick={()=> setCount(count=> count+1)}>{count} combo</button>
-          :<WalletButton/>}
+  return (
+    <div className="flex">
+      <main className="">
+        {account ? (
+          <button className={``} onClick={() => setCount((count) => count + 1)}>
+            {count} combo
+          </button>
+        ) : (
+          <WalletButton />
+        )}
+
+        <div className="text-red-700 text-4xl">test message</div>
       </main>
     </div>
   );
