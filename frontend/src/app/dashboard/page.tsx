b565ea97fbc4c1efc8e09fee8f1a7cadfaead864
Daegi Yeo <daegi0923@gmail.com>
2025-08-16 21:23:04 +0900

    feat : dashboard 추가

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import ChartTabs from "@/components/Dashboard/ChartTabs";
 import { useWalletAccountStore } from "@/app/hooks/auth.hooks";
 import Header from "@/components/Common/Header";
 import VaultStats from "@/components/Dashboard/VaultStats";
+import Composition from "@/components/Dashboard/Composition";
 
 export default function Dashboard() {
   const { account } = useWalletAccountStore();
@@ -16,10 +17,11 @@ export default function Dashboard() {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <main className="mx-auto w-full max-w-md flex-1 p-4">
+      <main className="mx-auto w-full max-w-md flex-1">
         <VaultStats tvl={tvl} apy={apy} timeLeft={timeLeft} />
         {/* 차트 탭 영역 */}
         <ChartTabs />
+        <Composition />
       </main>
     </div>
   );
