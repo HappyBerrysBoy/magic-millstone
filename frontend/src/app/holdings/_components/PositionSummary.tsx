d6823407b44add5445ecad6534e9699366cdb684
Nyangbari <hyojin.ju@gmx.net>
2025-08-21 17:40:46 +0900

    design: dashboard tab and holding summary

--- frontend/src/app/holdings/_components/PositionSummary.tsx
+++ frontend/src/app/holdings/_components/PositionSummary.tsx
@@ -11,62 +11,44 @@ export default function PositionSummary({
 }: PositionSummaryProps) {
   const totalValue = Number(balance) * Number(exchangeRate);
   return (
-    <>
-      <div>
-        <h1 className="mt-[30px] mb-[16px]">Your Position</h1>
-        <div className="flex flex-col gap-7">
-          <div className="flex flex-col">
-            <div>
-              <div className="flex items-baseline justify-between gap-3">
-                <p className="text-base font-normal">${totalValue}</p>
-                <p className="justify-between font-normal">USD</p>
-              </div>
+    <div className="flex w-full flex-col gap-7 pt-[50px]">
+      <h1 className="text-base font-medium">Your Position</h1>
+      <div className="flex w-full flex-col gap-9">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-[6px]">
+            <div className="flex items-baseline justify-between">
+              <p className="text-lg font-normal">${totalValue}</p>
+              <p className="text-mm-gray-default text-xs font-normal">USD</p>
             </div>
             <p className="text-mm-gray-light text-[10px] font-normal">
               Total value
             </p>
           </div>
-          {/* <div className="flex flex-col">
-            <div className="relative">
-              <p className="text-mm-gray-light absolute top-[-14px] text-[10px] font-normal">
-                $1.39
-              </p>
-              <div className="flex items-baseline justify-between gap-3">
-                <p className="text-base font-normal">$1.39</p>
-                <p className="text-base font-normal text-white">USDT</p>
-              </div>
+          <div className="bg-primary h-[0.3px] w-full" />
+          <div className="flex flex-col gap-[6px]">
+            <div className="flex items-baseline justify-between">
+              <p className="text-lg font-normal">{balance}</p>
+              <p className="text-sm font-normal text-white">mmUSDT</p>
             </div>
             <p className="text-mm-gray-light text-[10px] font-normal">
-              Pending Deposits
+              Your Balance
             </p>
-          </div> */}
-          <div className="flex flex-col">
-            <div>
-              <div className="flex items-baseline justify-between gap-3">
-                <p className="text-base font-normal">{balance}</p>
-                <p className="text-base font-normal text-white">mmUSDT</p>
-              </div>
-            </div>
-            <p className="text-gray text-[10px] font-normal">Your Balance</p>
           </div>
-          <div className="flex flex-col">
-            <div>
-              <div className="flex items-baseline justify-between gap-3">
-                <p className="text-base font-normal">{withdrawals}</p>
-                <p className="text-base font-normal text-white">mmUSDT</p>
-              </div>
+          <div className="bg-primary h-[0.3px] w-full" />
+          <div className="flex flex-col gap-[6px]">
+            <div className="flex items-baseline justify-between">
+              <p className="text-lg font-normal">{withdrawals}</p>
+              <p className="text-sm font-normal text-white">mmUSDT</p>
             </div>
-            <p className="text-gray text-[10px] font-normal">
+            <p className="text-mm-gray-light text-[10px] font-normal">
               Pending Withdrawals
             </p>
           </div>
         </div>
-        <div className="mt-[24px] w-full">
-          <p className="text-gray text-right text-sm">
-            1 mmUSDT = ${exchangeRate} USDT
-          </p>
-        </div>
+        <p className="text-mm-gray-default text-center text-xs">
+          1 mmUSDT = ${exchangeRate} USDT
+        </p>
       </div>
-    </>
+    </div>
   );
 }
